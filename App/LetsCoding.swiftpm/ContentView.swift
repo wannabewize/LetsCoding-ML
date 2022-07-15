b58e009738d965e1cd57619b73b90c9c86f3d8b9
Jaehoon Lee <wannabewize@gmail.com>
2022-07-16 03:15:19 +0900

    wip

--- App/LetsCoding.swiftpm/ContentView.swift
+++ App/LetsCoding.swiftpm/ContentView.swift
@@ -3,17 +3,18 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            ScrollView {
-                Text("Playground, SwiftUI!")
-            }
-            
-            NavigationButton(title: "Mercedes", destination: CClass())
-            NavigationButton(title: "스토리", destination: Page_1())
-            NavigationButton(title: "Machine Learning", destination: MLView())
-            NavigationButton(title: "Bio Diversity", destination: BioView())
-            NavigationButton(title: "Sample Page", destination: SampleView())
+            Page_Start()
+//            ScrollView {
+//                Text("Playground, SwiftUI!")
+//            }
+//
+//            NavigationButton(title: "Mercedes", destination: CClass())
+//            NavigationButton(title: "스토리", destination: Page_1())
+//            NavigationButton(title: "Machine Learning", destination: MLView())
+//            NavigationButton(title: "Bio Diversity", destination: BioView())
+//            NavigationButton(title: "Sample Page", destination: SampleView())
         }
-        .padding()
+        .padding(0)
         .navigationTitle("Lets Coding")
         .navigationBarTitleDisplayMode(.inline)
     }
--- App/LetsCoding.swiftpm/Page_Start.swift
+++ App/LetsCoding.swiftpm/Page_Start.swift
@@ -5,4 +5,23 @@
 //  Created by wannabewize on 2022/07/15.
 //
 
-import Foundation
+import SwiftUI
+
+struct Page_Start: View {
+    var body: some View {
+        ZStack(alignment: .bottom) {
+            Text("마지막 선택")
+                .fontWeight(.heavy)
+            GeometryReader { geometry in
+                BundleImage(fileName: "cover", ext: "png")
+                    .frame(width: geometry.size.width, alignment: .bottom)
+            .ignoresSafeArea(.container, edges: [.bottom, .leading, .trailing])
+            }
+
+        }
+        .background(Color.blue)
+
+        .navigationTitle("Sample Page")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
